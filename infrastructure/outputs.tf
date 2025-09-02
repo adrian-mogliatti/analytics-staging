fb9d183fc4b8909a3ef6dd92260c58ec425b22dd
adrian-mogliatti <adrian.mogliatti@gmail.com>
2025-09-01 23:01:56 -0300

    Update outputs.tf

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,1 +1,18 @@
+output "proxy_hostname" {
+  value = azurerm_app_service.proxy.default_site_hostname
+}
+
+output "backend_hostname" {
+  value = azurerm_app_service.backend.default_site_hostname
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+# Do not output the admin password here (sensitive) – it's available in Terraform state.
 
